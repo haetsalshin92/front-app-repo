cfffb50c6673c0f5df4ee73e2abdbf4877dff076
EC2 Default User <ec2-user@ip-172-31-45-162.ap-northeast-2.compute.internal>
2025-05-26 07:21:29 +0000

    사진 로컬에 넣는 형식으로 수정2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version ~~!!!!!
+            EKS DEMO Blog version ~~🍟!!!!!
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
