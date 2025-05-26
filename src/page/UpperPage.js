8ad482f249b22589fb52d9ad2e42c0e6d6a75aed
EC2 Default User <ec2-user@ip-172-31-45-162.ap-northeast-2.compute.internal>
2025-05-26 07:10:20 +0000

    사진 로컬에 넣는 형식으로 수정

--- src/page/UpperPage.js
+++ src/page/UpperPage.js
@@ -1,14 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
-import { Card, CardContent, CardActions } from '@material-ui/core';
-import { GridList } from '@material-ui/core';
-
-import { Typography } from '@material-ui/core';
-import { Button } from '@material-ui/core';
-
+import { Card, CardContent, CardActions, Typography, Button, GridList } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,32 +15,64 @@ const useStyles = makeStyles((theme) => ({
   gridList: {
     flexWrap: 'nowrap',
     transform: 'translateZ(0)',
-  }
+  },
 }));
 
+const data = {
+  outcome: [
+    {
+      name: "AWS",
+      url: "/images/demo-image-0.png",
+      value: "Amazon Web Services(AWS)",
+      link: "https://aws.amazon.com/ko/?nc2=h_lg"
+    },
+    {
+      name: "Amazon ECR",
+      url: "/images/demo-image-1.png",
+      value: "Amazon Elastic Container Registry",
+      link: "https://aws.amazon.com/ko/ecr/"
+    },
+    {
+      name: "Amazon EKS",
+      url: "/images/demo-image-2.png",
+      value: "Amazon Elastic Kubernetes Service",
+      link: "https://aws.amazon.com/ko/eks/"
+    },
+    {
+      name: "Amazon ECS",
+      url: "/images/demo-image-3.png",
+      value: "Amazon Elastic Container Service",
+      link: "https://aws.amazon.com/ko/ecs/"
+    },
+    {
+      name: "AWS Fargate",
+      url: "/images/demo-image-4.png",
+      value: "Serverless compute for containers",
+      link: "https://aws.amazon.com/ko/fargate/"
+    },
+    {
+      name: "Amazon Cloud9",
+      url: "/images/demo-image-5.png",
+      value: "A cloud IDE for writing, running, and debugging code",
+      link: "https://aws.amazon.com/ko/cloud9/"
+    },
+    {
+      name: "AWS CloudFormation",
+      url: "/images/demo-image-6.png",
+      value: "Speed up cloud provisioning with infrastructure as code",
+      link: "https://aws.amazon.com/ko/cloudformation/"
+    }
+  ]
+};
+
 function UpperPage() {
-    const classes = useStyles();
-    const [ data, setState ] = useState({outcome:[]})
-    
-    const url = 'http://k8s-eksdemogroup-e0353f9ab7-511897256.ap-northeast-2.elb.amazonaws.com/services/all'
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios(
-          url,
-        );
-        setState(result.data);
-      };
-      
-      fetchData();
-    }, []);
-  
-    
-    return (
-      <div className={classes.root}>
-        <GridList cellHeight={300} className={classes.gridList} cols={2}>
-          {data.outcome.map( item => (
-          <div>
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={300} className={classes.gridList} cols={2}>
+        {data.outcome.map((item, index) => (
+          <div key={index}>
             <Card className={classes.cardRoot}>        
               <CardContent>
                 <Typography color="textSecondary" gutterBottom>
@@ -66,16 +90,16 @@ function UpperPage() {
                 </Typography>
               </CardContent>
               <CardActions>
-                <a href={item.link}>
-                  <Button size="small"> See More </Button>
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  <Button size="small">See More</Button>
                 </a>
               </CardActions>
             </Card>
           </div>
-          ))}
-        </GridList>
-      </div>
-    )
+        ))}
+      </GridList>
+    </div>
+  );
 }
 
 export default UpperPage;
